886e93624c1491c25d50e41e41193febd5930988
Ruslan Stoyanov <ep8kha@gmail.com>
2018-02-06 13:53:40 +0500

    переименованы типы

--- gpio.c
+++ gpio.c
@@ -17,7 +17,7 @@
  * Устанавливает бит порта по маске
  * @param port Settable pin
  */
-void gpo_pin_bset(gpio_TypeDef* port) {
+void gpo_pin_bset(gpio_t* port) {
     port->gpio->BSRRL = port->gpio_mask;
     port->pin_state = true;
 }
@@ -26,7 +26,7 @@ void gpo_pin_bset(gpio_TypeDef* port) {
  * Сбрасывает бит порта по маске
  * @param port Resetable pin
  */
-void gpo_pin_brst(gpio_TypeDef* port) {
+void gpo_pin_brst(gpio_t* port) {
     port->gpio->BSRRH = port->gpio_mask;
     port->pin_state = false;
 }
@@ -35,7 +35,7 @@ void gpo_pin_brst(gpio_TypeDef* port) {
  * Устанавливает бит порта маске
  * @param port Settable pin
  */
-void gpo_pin_set(gpio_TypeDef* port) {
+void gpo_pin_set(gpio_t* port) {
     port->gpio->ODR |= port->gpio_mask;
     port->pin_state = true;
 }
@@ -44,7 +44,7 @@ void gpo_pin_set(gpio_TypeDef* port) {
  * Сбрасывает бит порта по маске
  * @param port Resetable pin
  */
-void gpo_pin_rst(gpio_TypeDef* port) {
+void gpo_pin_rst(gpio_t* port) {
     port->gpio->ODR &=~ port->gpio_mask;
     port->pin_state = false;
 }
@@ -53,7 +53,7 @@ void gpo_pin_rst(gpio_TypeDef* port) {
  * Меняет бит порта на противоположный по маске
  * @param port Invertible pin
  */
-void gpo_pin_tog(gpio_TypeDef* port) {
+void gpo_pin_tog(gpio_t* port) {
     port->gpio->ODR ^= port->gpio_mask;
     port->pin_state = gpo_pin_read(port);
 }
@@ -63,7 +63,7 @@ void gpo_pin_tog(gpio_TypeDef* port) {
  * @param port Readable out pin
  * @return Pin state
  */
-bool gpo_pin_read(gpio_TypeDef* port) {
+bool gpo_pin_read(gpio_t* port) {
     if((port->gpio->ODR & port->gpio_mask) == port->gpio_mask) return true;
     return false;
 }
@@ -73,7 +73,7 @@ bool gpo_pin_read(gpio_TypeDef* port) {
  * @param port Readable in pin
  * @return Pin state
  */
-bool gpi_pin_read(gpio_TypeDef* port) {
+bool gpi_pin_read(gpio_t* port) {
     if((port->gpio->IDR & port->gpio_mask) == port->gpio_mask) return true;
     return false;
 }
@@ -83,7 +83,7 @@ bool gpi_pin_read(gpio_TypeDef* port) {
  * @param port Readable in pin
  * @return Rise
  */
-bool gpi_pin_srd(gpio_TypeDef* port) { //soft rise detecting
+bool gpi_pin_srd(gpio_t* port) { //soft rise detecting
     bool cur_state = gpi_pin_read(port);
     if(cur_state & ~port->pin_state) {
         port->pin_state = cur_state;
@@ -98,7 +98,7 @@ bool gpi_pin_srd(gpio_TypeDef* port) { //soft rise detecting
  * @param port Readable in pin
  * @return Fall
  */
-bool gpi_pin_sfd(gpio_TypeDef* port) { //soft fall detectig
+bool gpi_pin_sfd(gpio_t* port) { //soft fall detectig
     bool cur_state = gpi_pin_read(port);
     if(~cur_state & port->pin_state) {
         port->pin_state = cur_state;
@@ -113,7 +113,7 @@ bool gpi_pin_sfd(gpio_TypeDef* port) { //soft fall detectig
  * @param port
  * @param data
  */
-void gpo_port_bset_by_mask(gpio_TypeDef* port,const gpio_t data) {
+void gpo_port_bset_by_mask(gpio_t* port,const port_t data) {
     port->gpio->BSRRL = (port->gpio_mask & data);
 }
 
@@ -122,7 +122,7 @@ void gpo_port_bset_by_mask(gpio_TypeDef* port,const gpio_t data) {
  * @param port
  * @param data
  */
-void gpo_port_brst_by_mask(gpio_TypeDef* port,const gpio_t data) {
+void gpo_port_brst_by_mask(gpio_t* port,const port_t data) {
     port->gpio->BSRRH = (port->gpio_mask & data);
 }
 
@@ -131,7 +131,7 @@ void gpo_port_brst_by_mask(gpio_TypeDef* port,const gpio_t data) {
  * @param port
  * @param data
  */
-void gpo_port_write_by_mask(gpio_TypeDef* port,const gpio_t data) {
+void gpo_port_write_by_mask(gpio_t* port,const port_t data) {
     gpo_port_brst_by_mask(port,~data);
     gpo_port_bset_by_mask(port,data);
 }
@@ -141,8 +141,8 @@ void gpo_port_write_by_mask(gpio_TypeDef* port,const gpio_t data) {
  * @param port
  * @param data
  */
-void gpo_port_range_write(gpio_TypeDef* port,const gpio_t data) {
-    gpio_t range_data = (data << port->shift);
+void gpo_port_range_write(gpio_t* port,const port_t data) {
+    port_t range_data = (data << port->shift);
     gpo_port_write_by_mask(port,range_data);
 }
 
@@ -151,8 +151,8 @@ void gpo_port_range_write(gpio_TypeDef* port,const gpio_t data) {
  * @param port
  * @return 
  */
-gpio_t gpo_port_range_read(gpio_TypeDef* port) {
-    gpio_t data = (port->gpio_mask & (port->gpio->ODR >> port->shift));
+port_t gpo_port_range_read(gpio_t* port) {
+    port_t data = (port->gpio_mask & (port->gpio->ODR >> port->shift));
     return data;
 }
 
@@ -161,7 +161,7 @@ gpio_t gpo_port_range_read(gpio_TypeDef* port) {
  * @param port
  * @return 
  */
-gpio_t gpi_port_range_read(gpio_TypeDef* port) {
-    gpio_t data = (port->gpio_mask & (port->gpio->IDR >> port->shift));
+port_t gpi_port_range_read(gpio_t* port) {
+    port_t data = (port->gpio_mask & (port->gpio->IDR >> port->shift));
     return data;
 }
--- gpio.h
+++ gpio.h
@@ -17,44 +17,44 @@
 #include <stm32f4xx.h>
 #include <stdbool.h>
 
-typedef uint16_t gpio_t;
+typedef uint16_t port_t;
 
 typedef struct {
     GPIO_TypeDef* gpio;
-    const gpio_t gpio_mask;
+    const port_t gpio_mask;
     const uint8_t shift;
     bool pin_state;
-} gpio_TypeDef;
+} gpio_t;
 
 
-extern void gpo_pin_bset(gpio_TypeDef* port);
+extern void gpo_pin_bset(gpio_t* port);
 
-extern void gpo_pin_brst(gpio_TypeDef* port);
+extern void gpo_pin_brst(gpio_t* port);
 
-extern void gpo_pin_set(gpio_TypeDef* port);
+extern void gpo_pin_set(gpio_t* port);
 
-extern void gpo_pin_rst(gpio_TypeDef* port);
+extern void gpo_pin_rst(gpio_t* port);
 
-extern void gpo_pin_tog(gpio_TypeDef* port);
+extern void gpo_pin_tog(gpio_t* port);
 
-extern bool gpo_pin_read(gpio_TypeDef* port);
+extern bool gpo_pin_read(gpio_t* port);
 
-extern bool gpi_pin_read(gpio_TypeDef* port);
+extern bool gpi_pin_read(gpio_t* port);
 
-extern bool gpi_pin_srd(gpio_TypeDef* port);
+extern bool gpi_pin_srd(gpio_t* port);
 
-extern bool gpi_pin_sfd(gpio_TypeDef* port);
+extern bool gpi_pin_sfd(gpio_t* port);
 
-extern void gpo_port_bset_by_mask(gpio_TypeDef* port, const gpio_t data);
+extern void gpo_port_bset_by_mask(gpio_t* port, const port_t data);
 
-extern void gpo_port_brst_by_mask(gpio_TypeDef* port, const gpio_t data);
+extern void gpo_port_brst_by_mask(gpio_t* port, const port_t data);
 
-extern void gpo_port_write_by_mask(gpio_TypeDef* port, const gpio_t data);
+extern void gpo_port_write_by_mask(gpio_t* port, const port_t data);
 
-extern void gpo_port_range_write(gpio_TypeDef* port, const gpio_t data);
+extern void gpo_port_range_write(gpio_t* port, const port_t data);
 
-extern gpio_t gpo_port_range_read(gpio_TypeDef* port);
+extern port_t gpo_port_range_read(gpio_t* port);
 
-extern gpio_t gpi_port_range_read(gpio_TypeDef* port);
+extern port_t gpi_port_range_read(gpio_t* port);
 
 #endif /* GPIO_H */
